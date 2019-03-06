0f6eaf98cf9ea1acb093c45454f38d8615915183
EOP <eop@sitecore.com>
2019-03-06 14:51:41 +0200

    Initial commit + default code

---
+++ src/Sitecore.Support.315261/Properties/AssemblyInfo.cs
@@ -0,0 +1,6 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Sitecore.Support.315261")]
+[assembly: AssemblyProduct("Sitecore.Support.315261")]
+[assembly: ComVisible(false)]
